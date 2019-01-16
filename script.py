9822df618c45a1ff0d617918fa0a36e4330dac1a
Brent Shierk <bshierk1996@gmail.com>
2019-01-16 16:06:55 -0500

    swagbucks info

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 # from selenium.webdriver.common.keys import keys
 import time 
+
 browser = webdriver.Chrome("C:\\Users\\uncor\\Desktop\\automation-project\\chromedriver.exe")
-browser.get('http://www.google.com/xhtml')
-time.sleep(5)  # Let the user actually see something!
-search_box = browser.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5)  # Let the user actually see something!
-browser.quit()
+
+def login(self,username,password,captchaloc):
+    browser.get('https://www.swagbucks.com/')
+    time.sleep(5)  # Let the user actually see something!
+    username = browser.find_element_by_id('sbxJxRegEmail').send_keys('pythontest667@gmail.com')
+    password = browser.find_element_by_id('sbxJxRegPswd').send_keys('roxxy123')
+    captchaloc = browser.find_element_by_class_name('recaptcha-checkbox-checkmark').click
+    time.sleep(20)  # Let the user actually see something!
+    browser.quit()
+
