28d6d2a6cf7b7d94391cbdf4bd2ca4eab20436da
Brent Shierk <bshierk1996@gmail.com>
2019-01-16 16:07:54 -0500

    change

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ def login(self,username,password,captchaloc):
     time.sleep(5)  # Let the user actually see something!
     username = browser.find_element_by_id('sbxJxRegEmail').send_keys('pythontest667@gmail.com')
     password = browser.find_element_by_id('sbxJxRegPswd').send_keys('roxxy123')
-    captchaloc = browser.find_element_by_class_name('recaptcha-checkbox-checkmark').click
+    captchaloc = browser.find_element_by_class_name('recaptcha-checkbox-checkmark').click(self)
     time.sleep(20)  # Let the user actually see something!
     browser.quit()
 
